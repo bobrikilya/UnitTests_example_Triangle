e11215847734a69d42094084da046d90dc6d0883
bobrikilya <ilya.bobrik.2020@gmail.com>
2022-02-12 22:45:50 +0300

    Filled main file and added exception

--- Area_calc.py
+++ Area_calc.py
@@ -3,6 +3,9 @@ from math import sqrt
 
 def area_calc(a, b, c):
     p = (a + b + c) / 2
-    return sqrt(p * (p - a) * (p - b) * (p - c))
+    try:
+        return sqrt(p * (p - a) * (p - b) * (p - c))
+    except ValueError:
+        raise ValueError("Такого треугольника не существует")
 
 # print(area_calc(2, 3, 0))
--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-#main
+from Type_of_triangle import type_finding
+from Area_calc import area_calc
+
+
+def main():
+    a = float(input("Введите a грань: "))
+    b = float(input("Введите b грань: "))
+    c = float(input("Введите c грань: "))
+    print(area_calc(a, b, c))
+    print(type_finding(a, b, c))
+
+
+if __name__ == '__main__':
+    main()
