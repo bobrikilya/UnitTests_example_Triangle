f104cfc1557fdb315055e29b10dc244f573c546b
bobrikilya <ilya.bobrik.2020@gmail.com>
2022-02-12 22:20:18 +0300

    Created type_finding fun

---
+++ Type_of_triangle.py
@@ -0,0 +1,14 @@
+def type_finding(a, b, c):
+    sides = [a, b, c]
+    sides.sort(reverse=True)
+    if a == b == c:
+        return "Равносторонний"
+    elif a == b != c or a == c != b or b == c != a:
+        return "Равнобедренный"
+    elif sides[0]**2 == sides[1]**2 + sides[2]**2:
+        return "Прямоугольный"
+    else:
+        return "Обычный"
+
+
+#print(type_finding(3, 4, 5))
